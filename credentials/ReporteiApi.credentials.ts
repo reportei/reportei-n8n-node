53aa93e70559096711d0392598893ac0a012ebdb
Eduardo Lelis <79928826+leliseduardo@users.noreply.github.com>
2025-04-08 16:24:29 -0300

    Integration credentials

---
+++ credentials/ReporteiApi.credentials.ts
@@ -0,0 +1,36 @@
+import {
+	IAuthenticateGeneric,
+	ICredentialType,
+	INodeProperties,
+} from 'n8n-workflow';
+
+export class ReporteiApi implements ICredentialType {
+	name = 'reporteiApi';
+	displayName = 'Reportei API';
+	documentationUrl = 'https://app.reportei.com/docs/api';
+
+	properties: INodeProperties[] = [
+		{
+			displayName: 'API Token',
+			name: 'apiToken',
+			type: 'string',
+			typeOptions: {
+				password: true,
+			},
+			default: '',
+			description: 'Enter your Reportei Bearer token here',
+			required: true,
+		},
+	];
+
+	// Deixe bem claro que o objeto atende ao IAuthenticateGeneric:
+	authenticate: IAuthenticateGeneric = {
+		type: 'generic',
+		properties: {
+			headers: {
+                ContentType: 'application/json',
+				Authorization: '=Bearer {{$credentials.apiToken}}',
+			},
+		},
+	};
+}
